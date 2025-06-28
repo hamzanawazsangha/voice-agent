f08e0d1dc65b51035c2d383182632fa05b02eb01
Muhammad Hamza Nawaz <iamhamzanawaz14@gmail.com>
2025-06-28 18:53:14 +0500

    Update faiss.py

--- faiss.py
+++ faiss.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.document_loaders import TextLoader
